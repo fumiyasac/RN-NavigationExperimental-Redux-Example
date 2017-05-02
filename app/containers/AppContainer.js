1ec7385a15d94aaa571b8d698aab7440a41b14cf
Fumiya Sakai <just1factory@gmail.com>
2017-05-02 23:51:55 +0900

    Delete Comment.

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -16,48 +16,31 @@ const {
 	Header: NavigationHeader
 } = NavigationExperimental
 
-
 class AppContainer extends React.Component {
 	render() {
 		let { navigationState, backAction } = this.props
 
 		return (
-
-			// Redux is handling the reduction of our state for us. We grab the navigationState 
-			// we have in our Redux store and pass it directly to the <NavigationTransitioner />.
 			<NavigationTransitioner
 				navigationState={navigationState}
 				style={styles.container}
-				render={props => (
-					// This mimics the same type of work done in a NavigationCardStack component
+				render={ props => (
 					<View style={styles.container}>
 						<NavigationCard
-							// <NavigationTransitioner>'s render method passes `navigationState` as a 
-							// prop to here, so we expand it plus other props out in <NavigationCard>.
 							{...props}
-							// Transition animations are determined by the StyleInterpolators. Here we manually
-							// override the default horizontal style interpolator that gets applied inside of 
-							// NavigationCard for a vertical direction animation if we are showing a modal.
-							// (Passing undefined causes the default interpolator to be used in NavigationCard.)
-							style={props.scene.route.key === 'Modal' ?
-										NavigationCard.CardStackStyleInterpolator.forVertical(props) :
-										undefined
-							}
+							style={props.scene.route.key === 'Modal' ? NavigationCard.CardStackStyleInterpolator.forVertical(props) : undefined}
 							onNavigateBack={backAction}
-							// By default a user can swipe back to pop from the stack. Disable this for modals.
-							// Just like for style interpolators, returning undefined lets NavigationCard override it.
-							panHandlers={props.scene.route.key === 'Modal' ? null : undefined }
+							panHandlers={props.scene.route.key === 'Modal' ? null : undefined}
 							renderScene={this._renderScene}
 							key={props.scene.route.key}
 						/>
 						<NavigationHeader
 							{...props}
 							onNavigateBack={backAction}
-							renderTitleComponent={props => {
+							renderTitleComponent={ props => {
 								const title = props.scene.route.title
 								return <NavigationHeader.Title>{title}</NavigationHeader.Title>
 							}}
-							// When dealing with modals you may also want to override renderLeftComponent...
 						/>
 					</View>
 				)}
@@ -67,7 +50,7 @@ class AppContainer extends React.Component {
 
 	_renderScene({scene}) {
 		const { route } = scene
-		
+
 		switch(route.key) {
 		case 'First':
 			return <First />
--- app/containers/AppContainerWithCardStack.js
+++ app/containers/AppContainerWithCardStack.js
@@ -1,7 +1,7 @@
 'use strict'
 
-import React, {PropTypes} from 'react'
-import {NavigationExperimental, StyleSheet} from 'react-native'
+import React, { PropTypes } from 'react'
+import { NavigationExperimental, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 
 import First from './First'
@@ -16,33 +16,16 @@ const {
 	Header: NavigationHeader
 } = NavigationExperimental
 
-
 class AppContainerWithCardStack extends React.Component {
 	render() {
 		let { navigationState, backAction } = this.props
 
 		return (
-
-			// Redux is handling the reduction of our state for us. We grab the navigationState 
-			// we have in our Redux store and pass it directly to the <NavigationCardStack />.
 			<NavigationCardStack
 				navigationState={navigationState}
 				onNavigateBack={backAction}
 				style={styles.container}
-				direction={navigationState.routes[navigationState.index].key === 'Modal' ?
-					'vertical' : 'horizontal'
-				}
-				renderHeader={props => (
-					<NavigationHeader
-						{...props}
-						onNavigateBack={backAction}
-						renderTitleComponent={props => {
-							const title = props.scene.route.title
-							return <NavigationHeader.Title>{title}</NavigationHeader.Title>
-						}}
-						// When dealing with modals you may also want to override renderLeftComponent...
-					/>
-				)}
+				direction={navigationState.routes[navigationState.index].key === 'Modal' ? 'vertical' : 'horizontal'}
 				renderScene={this._renderScene}
 			/>
 		)
@@ -50,7 +33,7 @@ class AppContainerWithCardStack extends React.Component {
 
 	_renderScene({scene}) {
 		const { route } = scene
-		
+
 		switch(route.key) {
 		case 'First':
 			return <First />
